dd009cb35413e8ebb34743f83116a3fac3c59fa3
suzandev <dev.suzanchandra@gmail.com>
2023-08-12 01:54:53 +0600

    some javaScript basic dom add

---
+++ script.js
@@ -0,0 +1,53 @@
+/*
+const liCollection = document.getElementsByTagName("li");
+// console.log(liCollection);
+
+for (const li of liCollection) {
+  console.log(li.innerText);
+}
+*/
+
+/*
+const allHeadings = document.getElementsByTagName("h1");
+for (const h1 of allHeadings) {
+  console.log(h1.innerText);
+}
+
+*/
+
+/*
+// ======== id Selection ========
+const idSelector = document.getElementById("fruits_title");
+console.log(idSelector);
+
+*/
+
+/*
+// =-=-=-=- Many Class Selection -=-=-=-=
+const classSelector = document.getElementsByClassName("important_places");
+// console.log(classSelector);
+
+for (const classSelect of classSelector) {
+  console.log(classSelect.innerText);
+}
+*/
+
+// =*=*=*=*=*=*=*= querySelector =*=*=*=*=*=
+
+/*
+const someLi = document.querySelectorAll(".fruits_container li");
+// console.log(someLi);
+for (const li of someLi) {
+  console.log(li.innerText);
+}
+
+*/
+
+/*
+// =-=-=-=-=- add a li to the places list id -=-=-=-=-
+const placesList = document.getElementById("places_list");
+const li = document.createElement("li");
+li.innerText = "pahartoli bon";
+
+placesList.appendChild(li);
+*/
